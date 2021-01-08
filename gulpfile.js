136ba770109e5bfbfaa3832eae3d9096b596a783
Rickard Nisses-Gagnér <rickard.nisses-gagner@skanska.se>
2021-01-08 10:09:58 +0100

    Added sass support

--- gulpfile.js
+++ gulpfile.js
@@ -18,11 +18,20 @@ gulp.task('pug', function () {
         .pipe(gulp.dest('./public'));
 });
 
+var sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+gulp.task('sass', function () {
+    return gulp.src('./source/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/styles'));
+});
+
 gulp.task('watch', function () {
     gulp.watch('source/html/**/*.html', gulp.series('html'));
     gulp.watch('source/pug/**/*.pug', gulp.series('pug'));
+    gulp.watch('source/sass/**/*.scss', gulp.series('sass'));
 });
 
-gulp.task('default', gulp.series('clean','pug','html', function (done) {
+gulp.task('default', gulp.series('clean','pug','sass','html', function (done) {
     done();
 }));
